fe4547285c23faca63e9ec4da6fbc2021a2b63d3
mdharb <>
2021-05-06 06:49:45 +0000

    modifying

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ var firstName=prompt('What is your first name?');
 
 var sureName=prompt('Your surename?');
 
-alert('Welcome'+" "+firstName+" "+sureName)
-
-var age=prompt('How old are you?');
+alert('Welcome'+" "+firstName+" "+sureName);
 
 var emailAddress=prompt('Could you please provide us with your email, so we can keep in touch about what is new in our website!');
 
 var destination=prompt('Your travel destination?');
 
+var age=prompt('How old are you?');
 
-if (age<18) {alert('Ops!!..you are less than 18 and you will not be able to continue browsing!');  
+if (age>18) {alert('Welcome to Travel+, we wish you a useful tour!');  
 }
-else { alert('Welcome to Travel+, we wish you a useful tour!');}
+else { alert('Ops!!..you are less than 18 and you will not be able to continue browsing!');}
+
